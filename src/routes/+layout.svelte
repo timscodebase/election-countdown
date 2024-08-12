c90a0b709d56e2e299353b352090325b98af5534
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-08-12 16:22:18 -0700

    style: Update meta tags and head section in +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,4 +11,25 @@
 
 <Analytics />
 
+<svelte:head>
+	<title>{name}</title>
+	<meta name="description" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta name="keywords" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta name="author" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="theme-color" content="#6712b8" />
+	<meta name="twitter:card" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta name="twitter:site" content="@timsmith23" />
+	<meta name="twitter:creator" content="@timsmith23" />
+	<meta property="og:title" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="/" />
+	<meta property="og:description" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta property="og:site_name" content="🇺🇸 Countdown to the Election 🇺🇸 2024" />
+	<meta property="og:image" content="desktop-screenshot.png" />
+	<link rel="apple-touch-icon" href="apple-touch-icon.png" />
+	<link rel="manifest" href="manifest.json" />
+	<link rel="canonical" href="/" />
+</svelte:head>
+
 {@render children()}
