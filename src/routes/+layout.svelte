0ec9b5b65431d51a36bb1b2dc9fcbec94bdf0b48
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-08-12 16:02:16 -0700

    Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
 	import Analytics from '$lib/analytics.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
