90b8a0564ee3eff97ee14c38cf837ce963495d5d
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-08-12 05:31:13 -0700

    feat: Add countdown for upcoming news debates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,17 @@
 	const lastElectionDay = new Date('2020-11-03');
 	const earlyVotingMailInStart = new Date('2024-09-06');
 	const earlyVotingInPersonStart = new Date('2024-09-20');
+	const foxNewsDebate = new Date('2024-09-04');
+	const abcNewsDebate = new Date('2024-09-10');
+	const nbcNewsDebate = new Date('2024-09-25');
 </script>
 
 <main>
-	<div class="countdown">
+	<section class="countdown">
 		<div class="election-day">
 			<h1>Days until Election Day</h1>
 			<p>{daysUntilFutureDate(electionDay)}</p>
-			<div class="days blue">Days</div>
+			<div class="days">Days</div>
 		</div>
 		<div class="wall"></div>
 		<div class="early-vote">
@@ -31,5 +34,24 @@
 			<p>{daysSinceDate(lastElectionDay)}</p>
 			<div class="days">Days</div>
 		</div>
-	</div>
+	</section>
+	<section class="countdown">
+		<h2 class="title">Debate Countdown</h2>
+		<div class="election-day">
+			<h3>Days until Fox News Debate</h3>
+			<p>{daysUntilFutureDate(foxNewsDebate)}</p>
+			<div class="days blue">Days</div>
+		</div>
+		<div class="wall"></div>
+		<div class="early-vote">
+			<h3>Days until ABC News Debate</h3>
+			<p>{daysUntilFutureDate(abcNewsDebate)}</p>
+			<div class="days blue">Days</div>
+			<hr />
+
+			<h3>Days until NBC News Debate</h3>
+			<p>{daysUntilFutureDate(nbcNewsDebate)}</p>
+			<div class="days blue">Days</div>
+		</div>
+	</section>
 </main>
