07659736cd8d9cc5bc2a31de5719899e3d0e1896
Tim Smith <69870077+timscodebase@users.noreply.github.com>
2024-08-23 13:32:42 -0700

    style: Refactor analytics script to use effect instead of reactive statement

--- src/lib/analytics.svelte
+++ src/lib/analytics.svelte
@@ -1,29 +1,26 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from '$app/stores'
 	// import gtag from 'gtag';
 
 	$effect(() => {
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3189326528459169"
-     crossorigin="anonymous"></script>
-
 		if (typeof gtag !== 'undefined') {
 			gtag('config', 'G-79ZYXTX8LL', {
 				page_title: document.title,
 				page_path: $page.url.pathname
-			})();
+			})
 		}
-	});
+	})
 </script>
 
 <svelte:head>
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-79ZYXTX8LL"></script>
 	<script>
-		window.dataLayer = window.dataLayer || [];
+		window.dataLayer = window.dataLayer || []
 		function gtag() {
-			dataLayer.push(arguments);
+			dataLayer.push(arguments)
 		}
-		gtag('js', new Date());
+		gtag('js', new Date())
 
-		gtag('config', 'G-79ZYXTX8LL');
+		gtag('config', 'G-79ZYXTX8LL')
 	</script>
 </svelte:head>
